c0e5fe163e98a53d81f704e101a9f8fc51bf8b11
Alex Chan <alex@alexwlchan.net>
2017-04-14 23:11:32 +0100

    Switch to editing the HTML string directly
    Turns out that BeautifulSoup doesn't always preserve the original
    HTML very well, and this can cause pages to be broken.  Editing
    the HTML with regexes is a bit more fiddly, but better at keeping
    the original intent.

--- archiver.py
+++ archiver.py
@@ -46,25 +46,35 @@ class HTMLArchiver:
         """
         Given a block of HTML, return a single-page HTML archive.
         """
-        soup = BeautifulSoup(html_string, 'html.parser')
-
         # Make sure there's a <meta charset="utf-8"> tag in the <head>,
         # because this uses UTF-8.
+        soup = BeautifulSoup(html_string, 'html.parser')
         head = soup.find('head')
         for meta_tag in head.find('meta'):
             if 'charset' in meta_tag.attrs:
                 break
         else:  # no break
-            meta_tag = soup.new_tag(name='meta')
-            meta_tag.attrs['charset'] = 'utf-8'
-            head.insert(0, meta_tag)
+            html_string = html_string.replace(
+                '<head>', '<head><meta charset="utf-8">')
 
-        soup = self._archive_js_scripts(soup, base_url=base_url)
-        soup = self._archive_style_tags(soup, base_url=base_url)
-        soup = self._archive_link_tags(soup, base_url=base_url)
-        soup = self._archive_img_tags(soup, base_url=base_url)
+        html_string = self._archive_js_scripts(
+            html_string=html_string,
+            soup=soup,
+            base_url=base_url)
+        html_string = self._archive_style_tags(
+            html_string=html_string,
+            soup=soup,
+            base_url=base_url)
+        html_string = self._archive_link_tags(
+            html_string=html_string,
+            soup=soup,
+            base_url=base_url)
+        html_string = self._archive_img_tags(
+            html_string=html_string,
+            soup=soup,
+            base_url=base_url)
 
-        return str(soup)
+        return html_string
 
     def _get_resource(self, url):
         if url in self.bad_urls:
@@ -98,7 +108,7 @@ class HTMLArchiver:
         encoded_string = base64.b64encode(resp.raw.read())
         return 'data:%s;base64,%s' % (media_type, encoded_string.decode())
 
-    def _archive_js_scripts(self, soup, base_url):
+    def _archive_js_scripts(self, html_string, soup, base_url):
         """
         Archive all the <script> tags in a soup.
         """
@@ -108,29 +118,67 @@ class HTMLArchiver:
             if js_tag.attrs.get('src') is None:
                 continue
 
-            new_tag = soup.new_tag(name='script')
-            resource_url = urljoin(base_url, js_tag.attrs['src'])
+            resource_url = urljoin(base_url, js_tag['src'])
 
             resp = self._get_resource(resource_url)
             if resp is None:
                 continue
 
+            match = re.search(
+                r'<script .*?src=(?P<qu>[\'"]?)%s(?P=qu)[^>]*></script>' % (
+                    re.escape(js_tag['src'])), html_string
+            )
+            assert match is not None, js_tag['src']
+
+            new_tag = soup.new_tag(name='script')
             new_tag.string = '\n' + resp.text.strip() + '\n'
             new_tag.attrs['type'] = 'text/javascript'
-            js_tag.replace_with(new_tag)
-        return soup
+            html_string = html_string.replace(match.group(0), str(new_tag))
+            assert match.group(0) not in html_string
+        return html_string
 
-    def _archive_style_tags(self, soup, base_url):
+    def _archive_style_tags(self, html_string, soup, base_url):
         """
-        Archive all the <style> tags in a soup.
+        Archive all the <style> tags and style attributes in a soup.
         """
         for style_tag in soup.find_all('style'):
-            css_string = style_tag.string
-            css_string = self.archive_css(css_string, base_url=base_url)
-            style_tag.string = css_string
-        return soup
+            orig_css_string = style_tag.string
+            css_string = self.archive_css(orig_css_string, base_url=base_url)
 
-    def _archive_link_tags(self, soup, base_url):
+            match = re.search(
+                r'<style(?P<attrs>[^>]+)>\s*?%s\s*?</style>' % (
+                    re.escape(orig_css_string)),
+                html_string
+            )
+            assert match is not None, orig_css_string
+            html_string = html_string.replace(
+                match.group(0),
+                '<style>\n' + css_string + '\n</style>'
+            )
+            assert match.group(0) not in html_string
+
+        for desc in soup.descendants:
+            try:
+                if desc.attrs.get('style') is None:
+                    continue
+            except AttributeError:
+                continue
+            orig_css_string = desc.attrs['style']
+            css_string = self.archive_css(orig_css_string, base_url=base_url)
+            if orig_css_string != css_string:
+                match = re.search(
+                    r'style=(?P<quot>[\'"]?)%s(?P=quot)' % (
+                        re.escape(orig_css_string)), html_string
+                )
+                assert match is not None, orig_css_string
+                html_string = html_string.replace(
+                    match.group(0), 'style=%s%s%s' % (
+                        match.group('quot'), css_string, match.group('quot')))
+                assert match.group(0) not in html_string
+
+        return html_string
+
+    def _archive_link_tags(self, html_string, soup, base_url):
         """
         Archive all the <link> tags in a soup.
         """
@@ -150,11 +198,19 @@ class HTMLArchiver:
             css_string = resp.text.strip()
             css_string = self.archive_css(css_string,
                 base_url=link_tag['href'])
-            style_tag.string = '\n' + css_string + '\n'
-            link_tag.replace_with(style_tag)
-        return soup
 
-    def _archive_img_tags(self, soup, base_url):
+            match = re.search(
+                r'<link .*?href=(?P<quot>[\'"]?)%s(?P=quot)[^>]*>' % (
+                    re.escape(link_tag['href'])), html_string
+            )
+            assert match is not None, link_tag['href']
+            style_tag.string = '\n' + css_string + '\n'
+            html_string = html_string.replace(match.group(0), str(style_tag))
+            assert match.group(0) not in html_string
+
+        return html_string
+
+    def _archive_img_tags(self, html_string, soup, base_url):
         """
         Archive all the <img> tags in a soup.
         """
@@ -164,8 +220,16 @@ class HTMLArchiver:
 
             resource_url = urljoin(base_url, img_tag['src'])
             data = self._get_base64_encode(resource_url)
+
+            match = re.search(
+                r'<img .*?src=(?P<quot>[\'"]?)%s(?P=quot)[^>]*>' % (
+                    re.escape(img_tag['src'])), html_string
+            )
+            assert match is not None, img_tag['src']
             img_tag['src'] = data
-        return soup
+            html_string = html_string.replace(match.group(0), str(img_tag))
+            assert match.group(0) not in html_string
+        return html_string
 
     def archive_css(self, css_string, base_url):
         """
